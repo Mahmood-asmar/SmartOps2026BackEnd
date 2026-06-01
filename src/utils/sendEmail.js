46354b525497403fee0ced38af9a2c91b7eda9f9
Mahmood Asmar <asmr7572@gmail.com>
2026-06-01 15:18:18 +0300

    Fix Gmail SMTP on Render

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -2,7 +2,10 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async ({ to, subject, text }) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    family: 4, // Force IPv4 because Render may fail with IPv6
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
