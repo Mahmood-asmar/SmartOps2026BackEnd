ad32ea98d42ff747b0334b606e7d4151824259f4
Mahmood Asmar <asmr7572@gmail.com>
2026-06-01 17:07:24 +0300

    Use Brevo API for sending OTP emails

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,14 +1,30 @@
-import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+import {
+  TransactionalEmailsApi,
+  SendSmtpEmail,
+} from "@getbrevo/brevo";
 
 const sendEmail = async ({ to, subject, text }) => {
-  await resend.emails.send({
-    from: process.env.EMAIL_FROM || "SmartOps <onboarding@resend.dev>",
-    to,
-    subject,
-    text,
-  });
+  const emailAPI = new TransactionalEmailsApi();
+
+  emailAPI.authentications.apiKey.apiKey = process.env.BREVO_API_KEY;
+
+  const message = new SendSmtpEmail();
+
+  message.sender = {
+    name: "SmartOps",
+    email: process.env.BREVO_SENDER_EMAIL,
+  };
+
+  message.to = [
+    {
+      email: to,
+    },
+  ];
+
+  message.subject = subject;
+  message.textContent = text;
+
+  await emailAPI.sendTransacEmail(message);
 };
 
 export default sendEmail;
